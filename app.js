470883be1c7c133ecca20f37ece8def120bfa2be
Udit Padhan <uditpadhan98@gmail.com>
2023-01-21 22:56:07 +0530

    completed login page

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ app.use(bodyParser.json());
 app.use(cookie());
 
 
-mongoose.set("strictQuery", false);
-// mongoose.set("useNewUrlParser",true);
+// mongoose.set("strictQuery", false);
+// // mongoose.set("useNewUrlParser",true);
 
-main().catch((err) => console.log(err));
+// main().catch((err) => console.log(err));
 
-async function main() {
-	await mongoose.connect("mongodb+srv://admin-naveen:Kkavn425@cluster0.hvrkrmt.mongodb.net/health-hist");
-	console.log("Connected");
-}
+// async function main() {
+// 	await mongoose.connect("mongodb+srv://admin-naveen:Kkavn425@cluster0.hvrkrmt.mongodb.net/health-hist");
+// 	console.log("Connected");
+// }
 
 
 const loginSchema = {
@@ -49,17 +49,45 @@ app.get("/",function(req,res){
 });
 
 
-app.post("/",function(req,res){
-    const email1 = req.body.email;
-    const password1 = req.body.password;
+// app.post("/",function(req,res){
+//     const email1 = req.body.email;
+//     const password1 = req.body.password;
 
-    const newlogindetail = new logindetail({
-        email : email1,
-        password : password1
-    })    
-    // newlogindetail.save();
+//     const newlogindetail = new logindetail({
+//         email : email1,
+//         password : password1
+//     })    
+//     // newlogindetail.save();
 
-}); 
+// }); 
+
+app.get("/LogIn",function(req,res){
+    res.render("LogIn");
+});
+
+app.get("/SignUp",function(req,res){
+    res.render("SignUp");
+});
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/Dashboard",function(req,res){
+    res.render("Dashboard");
+});
+
+app.get("/Department",function(req,res){
+    res.render("Department");
+});
+
+app.get("/Employees",function(req,res){
+    res.render("Employees");
+});
+
+app.get("/Appointment",function(req,res){
+    res.render("Appointment");
+});
 
 const PORT = process.env.PORT || 3000 ;
 app.listen(PORT,function(req,res){
